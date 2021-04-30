a0458c1b0fe06542291e7142c11ffcd8ee278b1e
Debora Ermel <debbyermel@gmail.com>
2021-04-30 15:02:30 -0500

    Adding key to lists

--- src/App.js
+++ src/App.js
@@ -12,8 +12,12 @@ function Header(props) {
 function MainSection(props) {
   return (
     <section>
-      <p>Click on any imagen to interect with this {props.adjective} it</p>
-    </section>
+      <p>Click on any of those {props.adjective} categories to start browsing</p>
+      <ul style={{ listStyle: "none" }}>
+        {props.categories.map((category) => (
+          <li key={category.id}>{category.title}</li>))}
+      </ul>
+    </section >
   );
 }
 
@@ -25,11 +29,21 @@ function Footer(props) {
   );
 }
 
+const categories = [
+  "Fashion",
+  "Travel",
+  "Minimal",
+  "Tech",
+  "Nature"
+];
+
+const categoriesObjects = categories.map((category, i) => ({ id: i, title: category }));
+
 function App() {
   return (
     <div className="App">
       <Header name="Cindy" />
-      <MainSection adjective="amazing" />
+      <MainSection adjective="amazing" categories={categoriesObjects} />
       <Footer year={new Date().getFullYear()} />
     </div>
   );
