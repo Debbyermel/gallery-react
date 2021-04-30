7072a90b39c9210483322ec52d014cb26c99ecd9
Debora Ermel <debbyermel@gmail.com>
2021-04-30 14:43:19 -0500

    Starting componnets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name} Gallery</h1>
+    </header>
+  );
+}
+
+function MainSection(props) {
+  return (
+    <section>
+      <p>Click on any imagen to interect with this {props.adjective} it</p>
+    </section>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p> Created my Deb| {props.year} copyright</p>
+    </footer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Cindy" />
+      <MainSection adjective="amazing" />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
