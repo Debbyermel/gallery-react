f5601da6be0b11e168efb9a7254e522a96aef650
Debora Ermel <debbyermel@gmail.com>
2021-05-27 15:28:53 -0500

    item component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,35 +1,13 @@
 import React from 'react';
+import TodoItem from "./TodoItem";
 
 export default function Todo() {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeOfDay;
-  const styles = {
-    display: "block",
-    fontSize: "16px"
-  }
-
-  if(hours > 12 && hours < 18) {
-    timeOfDay = "Afternoon"
-    styles.color = "#fea000"
-  }
-  else if (hours < 12) {
-    timeOfDay = "Morning"
-    styles.color = "#73bedb"
-  }
-  else {
-    timeOfDay = "Night"
-    styles.color = "#000"
-  }
 
   return (
-    <div>
-      <input type="checkbox" />
-      <p style={styles}>Good {timeOfDay}</p>
-      <input type="checkbox" />
-      <p style={styles}>Good {timeOfDay}</p>
-      <input type="checkbox" />
-      <p style={styles}>Good {timeOfDay}</p>
+    <div className="todo-list">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>
   )
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function TodoItem() {
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+  const styles = {
+    display: "block",
+    fontSize: "16px"
+  }
+
+  if(hours > 12 && hours < 18) {
+    timeOfDay = "Afternoon"
+    styles.color = "#fea000"
+  }
+  else if (hours < 12) {
+    timeOfDay = "Morning"
+    styles.color = "#73bedb"
+  }
+  else {
+    timeOfDay = "Night"
+    styles.color = "#000"
+  }
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p style={styles}>Good {timeOfDay}</p>
+    </div>
+  )
+}
+
