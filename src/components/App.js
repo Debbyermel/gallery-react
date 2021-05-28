6a6d99818003e84458d21195a595406a0d8c221d
Debora Ermel <debbyermel@gmail.com>
2021-05-27 19:45:34 -0500

    Adding States

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,37 @@ import Nav from "./Nav"
 import Footer from "./Footer"
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoggedIn: false,
+      answer: "Yes"
+    }
+  }
+
   render() {
+    let wordDisplay;
+    if(this.state.isLoggedIn === true) {
+      wordDisplay = "in";
+    }
+    else {
+      wordDisplay = "out";
+    }
+
     return (
       <div>
+        <p>Are you logged {wordDisplay}?</p>
         <Nav />
         <Home />
+        <h1>State is important? {this.state.answer}</h1>
         <Footer />
       </div>
-    );
+    )
   }
 }
 
 export default App;
+
 
 
 
